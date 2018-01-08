68cc38525fb2f80f6ef662dbf9931a46fb3c5470
josimar-junior <juniorpaz99@gmail.com>
2018-01-08 17:17:40 -0300

    Conclusion

--- src/com/br/jj/test/PaymentTest.java
+++ src/com/br/jj/test/PaymentTest.java
@@ -5,10 +5,13 @@ import static java.util.Arrays.asList;
 import java.math.BigDecimal;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
+import java.time.YearMonth;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -52,9 +55,68 @@ public class PaymentTest {
 	public void deveOrdenarEImprimirOsPagamentos() {
 		payments.stream().sorted(Comparator.comparing(Payment::getDate)).forEach(System.out::println);
 	}
-	
+
 	@Test
 	public void deveCalcularValorTotalPagamentoPayment1() {
 		payment1.getProducts().stream().map(Product::getPrice).reduce(BigDecimal::add).ifPresent(System.out::println);
+	}
+
+	@Test
+	public void deveCalcularValorTotalPagamentoTodosPayments() throws Exception {
+		System.out.println(payments.stream().flatMap(p -> p.getProducts().stream().map(Product::getPrice))
+				.reduce(BigDecimal.ZERO, BigDecimal::add));
+	}
+
+	@Test
+	public void deveMostrarOProdutoMaisVendido() throws Exception {
+		Map<Product, Long> topProducts = payments.stream().flatMap(p -> p.getProducts().stream())
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+		topProducts.entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).ifPresent(System.out::println);
+	}
+
+	@Test
+	public void deveMostrarValoresGeradosPorProduto() throws Exception {
+		Map<Product, BigDecimal> totalValorPorProduto = payments.stream().flatMap(p -> p.getProducts().stream())
+				.collect(Collectors.groupingBy(Function.identity(),
+						Collectors.reducing(BigDecimal.ZERO, Product::getPrice, BigDecimal::add)));
+
+		totalValorPorProduto.entrySet().stream().sorted(Comparator.comparing(Map.Entry::getValue))
+				.forEach(System.out::println);
+	}
+
+	@Test
+	public void deveMostrarOsProdutosDeCadaCliente() throws Exception {
+		Map<Customer, List<List<Product>>> listaProdutosPorCliente = payments.stream().collect(Collectors
+				.groupingBy(Payment::getCustomer, Collectors.mapping(Payment::getProducts, Collectors.toList())));
+
+		Map<Customer, List<Product>> listaProdutosPorCliente2 = listaProdutosPorCliente.entrySet().stream()
+				.collect(Collectors.toMap(Map.Entry::getKey,
+						e -> e.getValue().stream().flatMap(List::stream).collect(Collectors.toList())));
+
+		listaProdutosPorCliente2.entrySet().stream().sorted(Comparator.comparing(e -> e.getKey().getName()))
+				.forEach(System.out::println);
+	}
+
+	@Test
+	public void deveMostrarClienteEspecial() throws Exception {
+		Function<Payment, BigDecimal> somaTodosOsPrecos = p -> p.getProducts().stream().map(Product::getPrice)
+				.reduce(BigDecimal.ZERO, BigDecimal::add);
+
+		Map<Customer, BigDecimal> valorTotalPorCliente = payments.stream().collect(Collectors.groupingBy(
+				Payment::getCustomer, Collectors.reducing(BigDecimal.ZERO, somaTodosOsPrecos, BigDecimal::add)));
+
+		valorTotalPorCliente.entrySet().stream().sorted(Comparator.comparing(Map.Entry::getValue))
+				.forEach(System.out::println);
+	}
+
+	@Test
+	public void deveMostrarFaturamentoPorMes() throws Exception {
+		Map<YearMonth, BigDecimal> pagamentosPorMes = payments.stream()
+				.collect(Collectors.groupingBy(p -> YearMonth.from(p.getDate()), Collectors.reducing(BigDecimal.ZERO,
+						p -> p.getProducts().stream().map(Product::getPrice).reduce(BigDecimal.ZERO, BigDecimal::add),
+						BigDecimal::add)));
+
+		pagamentosPorMes.entrySet().stream().forEach(System.out::println);
 	}
 }
